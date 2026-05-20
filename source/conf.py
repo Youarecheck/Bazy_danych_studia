72d98b0f36f8eea9d16cdb2c71a3bf5ec369f4e3
Youarecheck <michal.kraus124@gmail.com>
2026-05-21 01:10:18 +0200

    Napisanie wstępu, wstępne generowanie pdf

--- source/conf.py
+++ source/conf.py
@@ -1,28 +1,30 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Sprawozdanie z laboratorium'
 copyright = '2026, Gal 2'
 author = 'Gal 2'
 release = '0.0.1'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
+# General configuration
+extensions = []  # ← USUŃ 'rst2pdf.pdfbuilder'
 
 templates_path = ['_templates']
 exclude_patterns = []
-
 language = 'pl'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+# HTML output
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+# LaTeX output (dla make latexpdf)
+latex_engine = 'pdflatex'
+latex_elements = {
+    'inputenc': r'\usepackage[utf8]{inputenc}',
+    'fontenc': r'\usepackage[T1]{fontenc}',
+    'babel': r'\usepackage[polish]{babel}',
+    'preamble': r'''
+        \usepackage{fncychap}
+        \usepackage{wrapfig}
+        \usepackage{enumitem}
+    ''',
+}
--- source/rozdzial_1/index.rst
+++ source/rozdzial_1/index.rst
@@ -1,5 +1,25 @@
-=======================================
 Wprowadzenie
-=======================================
+============
 
+Bazy danych to jedna z kluczowych gałęzi współczesnej informatyki. Dzięki odpowiedniej wiedzy
+i technologii możemy nie tylko gromadzić ogromne ilości danych, ale przede wszystkim
+**efektywnie je wykorzystywać**.
 
+W niniejszym dokumencie zagłębiamy się w tematykę baz danych, dokumentując nasze doświadczenia
+i eksperymenty przeprowadzone na testowych środowiskach. Ze względu na szeroki zakres zagadnień
+skupiamy się wyłącznie na podstawowych i niezbędnych wątkach.
+
+Technologie wybrane do opisu:
+
+* **SQLite** – lekka, bezserwerowa baza danych, idealna do prototypowania i zastosowań lokalnych,
+* **PostgreSQL** – zaawansowany, otwartoźródłowy system relacyjnych baz danych (RDBMS),
+  spełniający standard SQL oraz charakteryzujący się rozszerzalnością i niezawodnością.
+
+Ograniczenie do tych dwóch technologii wynika z ram czasowych i dydaktycznego charakteru opracowania.
+
+.. admonition:: Opracowanie
+   :class: note
+
+   **Autor:** Michał Kraus 
+   **Przedmiot:** Bazy danych  
+   **Data:** maj 2026
